02e02f0e09aed3f246b09eeab01746b44bad0e4b
brunof <brunof@unimedvaledocai.com.br>
2023-04-04 10:06:21 -0300

    Nova tela de register

---
+++ prisma/migrations/20230404122637_create_users/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "username" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
