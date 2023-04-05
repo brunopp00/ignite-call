c2c2c0c1e60ea59cf27d7006f2944805d15f88da
brunof <brunof@unimedvaledocai.com.br>
2023-04-05 14:28:21 -0300

    Alterações

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -40,6 +40,12 @@ export function buildNextAuthOption(
 
         return true
       },
+      async session({ session, user }) {
+        return {
+          ...session,
+          user,
+        }
+      },
     },
   }
 }
